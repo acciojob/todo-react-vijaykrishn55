4d6f07931e98bd909b0239ef6d54011bff3b528a
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-10-07 13:30:16 +0000

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
+
+
 const App = () => {
+  const [value,setValue]=useState("");
+  const [todo,setTodo]=useState([]);
+  const addTask=()=>{
+    setTodo([...todo,value]);
+    setValue("");
+  }
+  const deleteTodo=(index)=>{
+    let updatedTodo=[...todo];
+    updatedTodo.splice(index,1);
+    setTodo([...updatedTodo]);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>
+        <input type="text" onChange={(e)=>setValue(e.target.value)} value={value} />
+        <button onClick={addTask}>add Task</button>
+        </div>
+        <div>
+        <ul>
+          {todo.map((todos,index)=>(
+          <li key={index}>{todos}
+          <button onClick={()=>deleteTodo(index)}>delete</button>
+          </li>
+          ))}
+          </ul>
+        </div>
+        
     </div>
   )
 }
